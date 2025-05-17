b1df08f12275e5d0b9e5425fd5a24d6bc60ad54f
Alyonas999 <oranchuka@gmail.com>
2025-05-17 23:21:22 +0300

    finish

--- js/task-1.js
+++ js/task-1.js
@@ -1,11 +1,11 @@
   
 
-const categories = document.querySelectorAll('.card');
+const categories = document.querySelectorAll('.item');
   console.log(`Number of categories: ${categories.length}`);
 
   categories.forEach(category => {
-    const title= category.querySelector('.card-title').textContent;
-    const items = category.querySelectorAll('.card-item');
+    const title= category.querySelector('h2').textContent;
+    const items = category.querySelectorAll('li');
     
     console.log(`Category: ${title}`);
     console.log(`Elements: ${items.length}`);
--- js/task-3.js
+++ js/task-3.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const inputEl = document.querySelector('#name-input');
     const outputEl = document.querySelector('#name-output');
     inputEl.addEventListener('input', () => {
-        const trimmedValue = inputEl.ariaValueMax.trim();
+        const trimmedValue = inputEl.value.trim();
         outputEl.textContent = trimmedValue === '' ? 'Anonymous' : trimmedValue;
     });
 
